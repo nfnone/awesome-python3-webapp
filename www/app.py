3c3a0db98473173d97b7e3937a08e767b6bd3641
wangyue <wynfnone@gmail.com>
2019-07-12 13:58:23 +0300

    update app.py

--- www/app.py
+++ www/app.py
@@ -4,4 +4,37 @@ Created on Fri Jul 12 10:43:15 2019
 
 @author: ASUS
 """
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+async def index(request):
+    return web.Response(body='<h1>Awesome</h1>'.encode('utf-8'),content_type="text/html")
+
+def init():
+    app = web.Application()
+    app.add_routes([web.get('/', index)])
+    web.run_app(app, host='127.0.0.1', port=9000)
+# =============================================================================
+#     app = web.Application(loop=loop)
+#     app.router.add_route('GET', '/', index)
+#     srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+# =============================================================================
+    logging.info('server started at http://127.0.0.1:9000...')
+# =============================================================================
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(init(loop))
+# loop.run_forever()
+# =============================================================================
+init()
+
+
+
+
+
+
+
 
